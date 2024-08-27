018e164903f2ac024de0c3d0738d6d00e55e79e9
Karl <e.pereboev@gmail.com>
2024-08-27 22:05:48 +0300

    Изменения включают: Добавление данных

--- conftest.py
+++ conftest.py
@@ -1,1 +1,6 @@
- 
+import pytest
+
+@pytest.fixture(scope="session")
+def browser():
+    with pytest.raises(Exception, natch="could not start playwright"):
+        yield p.chromium.launch(headless=True)
