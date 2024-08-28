38301e7bd4496857cadcfdabf21c07ca2d46e3b5
Karl <e.pereboev@gmail.com>
2024-08-28 23:33:40 +0300

    Изменения включают: Добавление данных

--- base/pages/practice_form/practice_form_page.py
+++ base/pages/practice_form/practice_form_page.py
@@ -5,4 +5,10 @@ class PracticeFormPage:
     def __init__(self, page: Page) -> None:
         self.page = page
 
-        self.first_name = self.page.locator()
+        """Локаторы страницы: Форма"""
+        self.first_name = input(page, locator='//*[@id="firstName"]', name='Имя')
+        self.last_name = input(page, locator='//*[@id="lastName"]', name='Фамилия')
+
+        """Ожидание"""
+        self.Wait_first_name = '//*[@id="firstName"]'
+        self.Wait_last_name = '//*[@id="lastName"]'
