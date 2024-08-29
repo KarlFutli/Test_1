d0e407778ac8c9ff94e9490c87167a98ce0930c3
Karl <e.pereboev@gmail.com>
2024-08-29 22:38:45 +0300

    Изменения включают: Добавление данных

--- base/pages/practice_form/practice_form_page.py
+++ base/pages/practice_form/practice_form_page.py
@@ -1,14 +1,14 @@
 from playwright.sync_api import Page
-
+from base.page_factory.input import Input
 
 class PracticeFormPage:
     def __init__(self, page: Page) -> None:
         self.page = page
 
         """Локаторы страницы: Форма"""
-        self.first_name = input(page, locator='//*[@id="firstName"]', name='Имя')
-        self.last_name = input(page, locator='//*[@id="lastName"]', name='Фамилия')
+        self.first_name = Input(page, locator='//*[@id="firstName"]', name='Имя')
+        self.last_name = Input(page, locator='//*[@id="lastName"]', name='Фамилия')
 
-        """Ожидание"""
+        """Ожидания"""
         self.Wait_first_name = '//*[@id="firstName"]'
         self.Wait_last_name = '//*[@id="lastName"]'
